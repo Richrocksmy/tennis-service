d769f045bd40b79e80e1da0d614a803ca6c6beb3
Rich Jeffrey <rich@Richs-Mac-mini.local>
2021-06-20 10:13:36 +0100

    Add another resource test with H2 dependency to keep service db connection happy

--- src/test/java/org/richrocksmy/tennisservice/tournament/TournamentResourceTest.java
+++ src/test/java/org/richrocksmy/tennisservice/tournament/TournamentResourceTest.java
@@ -1,18 +1,37 @@
 package org.richrocksmy.tennisservice.tournament;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.richrocksmy.tennisservice.match.CreateMatchRequest;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 import java.time.ZonedDateTime;
 import java.util.Set;
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @QuarkusTest
 public class TournamentResourceTest {
 
-//    @Test
+    private Jsonb jsonb;
+
+    @InjectMock
+    private TournamentService tournamentService;
+
+    @BeforeEach
+    public void setup() {
+        jsonb = JsonbBuilder.create();
+    }
+
+    @Test
     public void testCreateTournamentEndpoint() {
+        when(tournamentService.createTournament(any())).thenReturn(UUID.randomUUID());
         CreateTournamentRequest createTournamentRequest = new CreateTournamentRequest();
         createTournamentRequest.setStartDate(ZonedDateTime.now());
 
@@ -22,10 +41,10 @@ public class TournamentResourceTest {
         createMatchRequest.setPlayerB("Player B");
         createTournamentRequest.setMatches(Set.of(createMatchRequest));
 
-        given().body(createTournamentRequest)
+        given().contentType("application/json").body(jsonb.toJson(createTournamentRequest))
             .when().post("/v1/tournaments")
             .then()
-            .statusCode(200);
+            .statusCode(201);
     }
 
 }
